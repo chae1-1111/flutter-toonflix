e3adcea3aa79638edfcec720899ad802b08b0369
chae1-1111 <dla4212@naver.com>
2023-02-19 14:01:52 +0900

    edit to get webtoon list in future builder

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,16 +1,25 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
 
-  final String today = "today";
+  static const String today = "today";
 
-  void getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final uri = Uri.parse("$baseUrl/$today");
     final response = await http.get(uri);
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        final toon = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(toon);
+      }
+      return webtoonInstances;
     }
     throw Error();
   }
